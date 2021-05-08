47770ab61df6ed16cb4bc780cff23f9b16cbd92d
noob <coniferchina@gmail.com>
2021-05-08 14:39:00 +0800

    add ethereum method

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,34 +7,45 @@ export default {
       app.component('ambre-' + item.substring(2, item.lastIndexOf('.')).toLowerCase(), file)
     })
   },
-  format(timestamp: number, format: string = 'yyyy-MM-dd hh:mm:ss') {
-    // const date = new Date(timestamp)
-    // if (/(y+)/.test(format)) {
-    //   format = format.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
-    // }
-    // const object = {
-    //   'M+': date.getMonth() + 1,
-    //   'd+': date.getDate(),
-    //   'h+': date.getHours(),
-    //   'm+': date.getMinutes(),
-    //   's+': date.getSeconds()
-    // }
-    // for (const key in object) {
-    //   if (new RegExp(`(${key})`).test(format)) {
-    //     const string = JSON.stringify(object[key]) + ''
-    //     format = format.replace(
-    //       RegExp.$1,
-    //       RegExp.$1.length === 1 ? string : ('00' + string).substr(string.length)
-    //     )
-    //   }
-    // }
-    return format
-  },
+  // format(timestamp: number, format: string = 'yyyy-MM-dd hh:mm:ss') {
+  //   console.log(timestamp, format)
+  //   const date = new Date(timestamp)
+  //   if (/(y+)/.test(format)) {
+  //     format = format.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+  //   }
+  //   const object = {
+  //     'M+': date.getMonth() + 1,
+  //     'd+': date.getDate(),
+  //     'h+': date.getHours(),
+  //     'm+': date.getMinutes(),
+  //     's+': date.getSeconds()
+  //   }
+  //   for (const key in object) {
+  //     if (new RegExp(`(${key})`).test(format)) {
+  //       const string = JSON.stringify(object[key]) + ''
+  //       format = format.replace(
+  //         RegExp.$1,
+  //         RegExp.$1.length === 1 ? string : ('00' + string).substr(string.length)
+  //       )
+  //     }
+  //   }
+  //   return format
+  // },
   output: (path: string) => ({
     filename: 'index.js',
     path,
     publicPath: ''
   }),
+  ethereum: async () => {
+    const ethereum = window.ethereum
+    if (ethereum) {
+      // console.log('MetaMask is installed!')
+      // console.log(ethereum.isMetaMask)
+      const [accounts] = await ethereum.request({ method: 'eth_requestAccounts' })
+      return accounts
+    }
+  },
+
   html: () => ({
     title: 'ambre-vue-cli-next',
     filename: 'index.html',
